31793048557d67a64578764bb86f74baa12a8b19
Max Marcussen <axemanscrums@cs.uchicago.edu>
2021-05-27 16:53:33 -0500

    cuda is working, let's make algorithm

--- hist_coal.cu
+++ hist_coal.cu
@@ -1,56 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-__global__ void hist_kernel(float* d_f, int sz, int* d_counts, int rstart, int rend, int nbins) {
-    for (int i = threadIdx.x + blockIdx.x * blockDim.x; i < sz; i += blockDim.x * gridDim.x) {
-	float c = d_f[i];
-        int place = 0;
-	float fnbins = (float) nbins;
-	float dist = (float) rend-rstart;
-	float binsize = dist / fnbins;
-	//printf("%f ", c);
-	// getting way too many zeros for some reason. I'll add back the missing
-	// number of zeros later
-	if (c >= rstart && c < rend && c != 0) {
-	    //printf("%f\n", c);
-            for (int j = 0 ; j < nbins; j++) {
-                if (c >= rstart + binsize * j && c < rstart + binsize * (j+1)) {
-		    //printf("%f\n", c)
-		    atomicAdd(&d_counts[place], 1);
-		    break;
-	        } else {
-		    place += 1;
-	        }
-            }
-	}
-	//atomicAdd(&d_counts[place], 1);
-    }
-}
-
-void print_hist(int* counts, int rstart, int rend, int nbins, int sz) {
-    float fnbins = (float) nbins;
-    float dist = (float) rend-rstart;
-    float binsize = dist / fnbins;
-    for (int i = 0; i < nbins; i++) {
-        printf("Bin %i [%0.6f, %0.6f)", i, rstart+binsize*i, rstart+binsize*(i+1));
-	if (0 >= rstart+binsize*i && 0 < rstart+binsize*(i+1)) {
-	    printf(": %i values\n", counts[i]);
-	} else {
-	    printf(": %i values\n", counts[i]);
-	}
+__global__ void hist_kernel(char* d1, char* d2, char* dmatch1, char* dmatch2, int sz1, int sz2) {
+    for (int i = threadIdx.x + blockIdx.x * blockDim.x; i < sz1; i += blockDim.x * gridDim.x) {
+	for (int j = threadIdx.y + blockIdx.y * blockDim.y; j < sz2; j += blockDim.y * gridDim.y) {
+	    if (d1[i] == d2[j]) {
+	        printf("Match found\n");
+		printf("Indices %d %d \n", i, j);
+	    }
+	}	
     }
 }
 
 int main(int argc, char* argv[]){
     FILE *seq1;
     FILE *seq2;
-    //int grid;
-    //int block;
+    int grid;
+    int block;
     if (argc == 5) {
         seq1 = fopen(argv[1], "rb");
 	seq2 = fopen(argv[2], "rb");
-        //grid = atoi(argv[3]);
-	//block = atoi(argv[4]);
+        grid = atoi(argv[3]);
+	block = atoi(argv[4]);
     }
     else {
         printf("Incorrect number of arguments. Arguments should take form: sequence 1, sequence 2, grid dim, block dim.\n");
@@ -62,47 +33,52 @@ int main(int argc, char* argv[]){
     fgets(name1, 128, seq1);
     fgets(name2, 128, seq2);
     fseek(seq1, 0L, SEEK_END);
-    int sz1 = ftell(seq1);
+    int sz1 = ftell(seq1)-strlen(name1)-1;
     fseek(seq2, 0L, SEEK_END);
-    int sz2 = ftell(seq2);
+    int sz2 = ftell(seq2)-strlen(name2)-1;
     printf("%d %d\n", sz1, sz2);
-    printf("%d", strlen(name1));
-    /*
-    fseek(in_file, 0L, SEEK_SET);
-    float f1[sz];
-    fread(f1, sizeof(float), sz, in_file);
-    //printf("%i\n", sz);
-    float f[sz/4];
-    memcpy(f, f1, sz);
+    // sz1 and sz2 are the sizes of the sequences after the first line
+
+    // get back to start of sequence
+    fseek(seq1, 0L, SEEK_SET);
+    fseek(seq2, 0L, SEEK_SET);
+    fgets(name1, 128, seq1);
+    fgets(name2, 128, seq2);
+ 
+    char* text1 = (char*)malloc(sz1*sizeof(char));
+    char* text2 = (char*)malloc(sz1*sizeof(char));
+    fread(text1, sizeof(char), sz1, seq1);
+    fread(text2, sizeof(char), sz2, seq2);
     
-    float* d_f;
-    cudaMalloc((void**)&d_f, sz);
-    cudaMemcpy(d_f, f, sz, cudaMemcpyHostToDevice);
+    // printf("%c", text1[6]);
+    
+    // data is all read in!
+    // now time to get cuda working
+    // create data in CUDA for both texts and for best matchstrs   
+    char* d1;
+    cudaMalloc((void**)&d1, sz1*sizeof(char));
+    cudaMemcpy(d1, text1, sz1, cudaMemcpyHostToDevice);
+    char* d2;
+    cudaMalloc((void**)&d2, sz2*sizeof(char));
+    cudaMemcpy(d2, text2, sz2, cudaMemcpyHostToDevice);
+ 
+    char* match1 = (char*) malloc(sz1*sizeof(char));
+    char* match2 = (char*) malloc(sz2*sizeof(char));
+    char* dmatch1;
+    char* dmatch2;
+    cudaMalloc((void**)&dmatch1, sz1*sizeof(char));
+    cudaMalloc((void**)&dmatch2, sz2*sizeof(char));
 
-    int * counts = (int*) malloc(nbins*sizeof(int));
-    int * d_counts;
-    cudaMalloc((void**)&d_counts, nbins*sizeof(int));
-    cudaMemset(d_counts, 0, nbins*sizeof(int));
-
+    // create start and end events
     cudaEvent_t tick, tock;
     cudaEventCreate(&tick);
     cudaEventCreate(&tock);
-
+    // call funciton and report time
     cudaEventRecord(tick, 0);
-    hist_kernel<<<grid, block>>>(d_f, sz, d_counts, rstart, rend, nbins);
+    hist_kernel<<<grid, block>>>(d1, d2, dmatch1, dmatch2, sz1, sz2);
     cudaEventRecord(tock, 0);
     cudaEventSynchronize(tock);
     float time;
     cudaEventElapsedTime(&time, tick, tock);
-
-    cudaMemcpy(counts, d_counts, nbins*sizeof(int), cudaMemcpyDeviceToHost);
-
-    print_hist(counts, rstart, rend, nbins, sz);
     printf("time elapsed: %0.6f ms\n", time);
-
-    cudaEventDestroy(tick);
-    cudaEventDestroy(tock);
-    cudaFree(d_counts);
-    cudaFree(d_f);
-    */
 }
